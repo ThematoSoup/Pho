d3fa59eb45724ada6480eb173efa06022d7e181a
Slobodan Manic <slobodan.manic@gmail.com>
2014-07-07 11:06:49 +0200

    Removes custom header (header image) functionality

--- functions.php
+++ functions.php
@@ -223,11 +223,6 @@ jQuery( document ).ready( function( $ ) {
 add_action( 'wp_footer', 'pho_masonry_init', 100 );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';

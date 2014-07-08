9627c1ebc9bd84a1503a92cdbd66b3e6c2787234
Slobodan Manic <slobodan.manic@gmail.com>
2014-07-08 05:11:50 +0200

    Fixes #13

--- footer.php
+++ footer.php
@@ -17,18 +17,18 @@
 
 		<div id="footer-bottom">
 			<div class="inner">
-				<div class="site-info">
-					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'pho' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'pho' ), 'WordPress' ); ?></a>
-					<span class="sep"> | </span>
-					<?php printf( __( 'Theme: %1$s by %2$s.', 'pho' ), 'Pho', '<a href="http://thematosoup.com" rel="designer">ThematoSoup</a>' ); ?>
-				</div><!-- .site-info -->
-
 				<?php wp_nav_menu( array(
 					'theme_location' => 'footer',
 					'depth' => 1,
 					'fallback_cb' => '',
 					'container' => ''
 				) ); ?>
+
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'pho' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'pho' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+					<?php printf( __( 'Theme: %1$s by %2$s.', 'pho' ), 'Pho', '<a href="http://thematosoup.com" rel="designer">ThematoSoup</a>' ); ?>
+				</div><!-- .site-info -->
 			</div>
 		</div>
 		</div>
